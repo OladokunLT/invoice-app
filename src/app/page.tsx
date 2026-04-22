6e6f5cc07c3a5c1bfe810e02c5c6f34431cdd048
oladokunlt <lukmanoladokun86@gmail.com>
2026-04-22 21:48:59 +0100

    Setup Next.js with TypeScrpt, Tailwindcss v4 and project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold text-blue-600">
+        Invoice App — Setup Complete 🚀
+      </h1>
+    </main>
+  );
+}
