412d066564cb81bfbd901b14f6dc4b9ffab49e35
oladokunlt <lukmanoladokun86@gmail.com>
2026-04-23 15:03:22 +0100

    Refactor Filter component to include dropdown functionality and close on outside click

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import { InvoiceStatus } from "@/types/invoice";
 interface Props {
   selected: InvoiceStatus[];
@@ -7,6 +8,9 @@ interface Props {
 }
 
 export default function Filter({ selected, setSelected }: Props) {
+  const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
   const options: InvoiceStatus[] = ["draft", "pending", "paid"];
 
   const toggle = (status: InvoiceStatus) => {
@@ -17,19 +21,50 @@ export default function Filter({ selected, setSelected }: Props) {
     }
   };
 
+  //   Close on outside click
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (!ref.current?.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
+
   return (
-    <div className="flex gap-3">
-      {options.map((status) => (
-        <label key={status} className="flex items-center gap-2 cursor-pointer">
-          <input
-            type="checkbox"
-            checked={selected.includes(status)}
-            onChange={() => toggle(status)}
-            title="filter"
-          />
-          <span className="capitalize">{status}</span>
-        </label>
-      ))}
+    <div className="relative" ref={ref}>
+      {/* Button */}
+      <button
+        onClick={() => setOpen(!open)}
+        className="font-medium flex items-center gap-2 cursor-pointer"
+      >
+        Filter by status
+        <span className={`transition ${open ? "rotate-180" : ""}`}>▼</span>
+      </button>
+
+      {/* Dropdown */}
+      {open && (
+        <div className="absolute right-0 mt-3 w-48 bg-white dark:bg-[#1E2139] shadow-lg p-4 space-y-3 z-50">
+          {options.map((status) => (
+            <label
+              key={status}
+              className="flex items-center gap-3 cursor-pointer group"
+            >
+              <input
+                type="checkbox"
+                checked={selected.includes(status)}
+                onChange={() => toggle(status)}
+                className="accent-purple-600"
+              />
+              <span className="capitalize group-hover:text-purple-500">
+                {status}
+              </span>
+            </label>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
